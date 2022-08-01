2bb4b635babd790a21a836be82c1da72a22f31ce
ehasanulh1 <ehasanulh1@gmail.com>
2022-08-01 22:16:11 +0600

    solced 1 and 2

---
+++ practice-problem-1.js
@@ -0,0 +1,13 @@
+var fruits = ['apple', 'banana', 'orange'];
+
+//Array element change/replace by index
+fruits[1] = 'mango'
+console.log(fruits)
+
+//Array element remove by pop (! last element)
+fruits.pop()
+console.log(fruits)
+
+//Array element add by push keyword (! last element)
+fruits.push('watermelon')
+console.log(fruits)
---
+++ practice-problem-2.js
@@ -0,0 +1,28 @@
+var me = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+// // var score = me;
+// // var score = tom;
+// // var score = jane;
+var score = peter;
+// var score = john;
+
+
+if (score >= 80) {
+    console.log('You got: A grade')
+}
+else if (score >= 60) {
+    console.log('You got: B grade')
+}
+else if (score >= 50) {
+    console.log('You got: C grade')
+}
+else if (score >= 40) {
+    console.log('You got: D grade')
+}
+else if (score <= 39) {
+    console.log('You got: F grade')
+}
